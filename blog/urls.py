314657b15016531f22a498938611bfacebca7754
kimsangwon <tkddnjs2014@gmail.com>
2019-07-29 20:20:18 +0900

    commits

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,6 @@ from . import views #blog views 가져오기
 
 urlpatterns = [
     path('', views.post_list, name='post_list'), #/blog/ url로 접속하면 view의 post_list 보여줌
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    # <int: pk>: 장고는 정수 값을 기대하고 이를 pk라는 변수로 뷰로 전송
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render  # render import
 from .models import Post  # models 함수의 Post 객체 불러오기
 from django.utils import timezone #timzone import
+from django.shortcuts import render, get_object_or_404 # page not found page 제공
 
 
 def post_list(request):
@@ -10,3 +11,8 @@ def post_list(request):
 # post_list 함수: request를 넘겨받아 rener 메소드 호출
 # rednder 함수: blog/post_list.html 템플릿을 호출
 # posts: 쿼리셋의 이름
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk) #url로 부터 받은 pk 값에 해당하는 post가 없을 경우 page not found 제
+    return render(request, 'blog/post_detail.html', {'post': post})
